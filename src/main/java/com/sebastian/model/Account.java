7e1184f0888ff39797968c33516fd902d7209c5d
juanse217 <juansebas08004@gmail.com>
2025-09-03 16:52:26 -0500

    basic implementation

--- src/main/java/com/sebastian/model/Account.java
+++ src/main/java/com/sebastian/model/Account.java
@@ -1,5 +1,34 @@
 package com.sebastian.model;
 
 public class Account {
+    private String accountNumber; 
+    private double balance; 
+    private String ownerName;
     
+    public Account(String accountNumber, double balance, String ownerName) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.ownerName = ownerName;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    } 
+
+    public void addBalance(double amount){
+        balance += amount;
+    }
+
+    public boolean withdraw(double amount){
+        if(amount <= balance){
+            balance -= amount;
+            return true; 
+        }
+
+        return false; 
+    }
 }
--- src/main/java/com/sebastian/service/Atm.java
+++ src/main/java/com/sebastian/service/Atm.java
@@ -1,5 +1,50 @@
 package com.sebastian.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+
+import com.sebastian.exceptions.AccountNotFoundException;
+import com.sebastian.model.Account;
+
 public class Atm {
-    
+    private static final Logger logger = (Logger) LogManager.getLogger(Atm.class);
+    private List<Account> accounts;
+
+    public Atm() {
+        accounts = new ArrayList<>();
+    }
+
+    private Optional<Account> findAccount(String accNumber) {
+       return accounts.stream().filter(x -> x.getAccountNumber().equals(accNumber)).findFirst();
+    }
+
+    public void addBalance(String accountNumber, double balance) throws AccountNotFoundException {
+        Optional<Account> acc = findAccount(accountNumber);
+        
+        if(acc.isEmpty()){
+            throw new AccountNotFoundException("The account wasn't found to add balance");
+        }else{
+            acc.get().addBalance(balance);
+            System.out.println("Succesful adding");
+        }
+    }
+
+    public void withdraw(String accountNumber, double amount) throws AccountNotFoundException {
+        Optional<Account> acc = findAccount(accountNumber);
+
+        if(acc.isEmpty()){
+            throw new AccountNotFoundException("The account wasn't found for the withdraw");
+        }else{
+            if(acc.get().withdraw(amount)){
+                System.out.println("Succesful withdraw");
+            }else{
+                System.out.println("Not enough balance");
+            }
+        }
+    }
 }
